a65dbc5d705397b5cac34cc0c26f7a5349136ef8
yeswadams <yeswaadams73@gmail.com>
2025-08-23 00:35:35 +0300

    Fixed the Routes bug in the app.tsx and main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThirdwebProvider } from "thirdweb/react";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThirdwebProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThirdwebProvider>
   </React.StrictMode>
 );
